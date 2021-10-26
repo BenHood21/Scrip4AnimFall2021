d889d64cbc178bb95dd3d77301521f485bb8c6ff
Benjamin Hood <10885066@my.uvu.edu>
2021-10-26 17:01:03 -0600

    Art and UI added

--- CosmoCrashCorse3D/Assets/Script/PlayerBehaviour.cs
+++ CosmoCrashCorse3D/Assets/Script/PlayerBehaviour.cs
@@ -4,21 +4,38 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float playerSpeed;
+    public Joystick joystick;
+    private float horizontalMove = 5f;
+    
     private Rigidbody rb;
-    private Vector2 playerDirection;
+   private Vector2 playerDirection;
+    
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
-    { 
-        float directionX = Input.GetAxisRaw("Horizontal"); 
-        playerDirection = new Vector3(directionX,0,0).normalized;
+    { // Youtube Tutorial: TOUCH CONTROLS in Unity! by Brackeys
+        if (joystick.Horizontal >= .2f)
+        {
+            horizontalMove = playerSpeed;
+        }
+        else if (joystick.Horizontal <= -.2f)
+        {
+            horizontalMove = -playerSpeed;
+        }
+        else
+        {
+            horizontalMove = 0f;
+        }
+       
+        playerDirection = new Vector3(horizontalMove,0,0).normalized;
     }
 
     private void FixedUpdate()
     {
+       
         rb.velocity = new Vector3(playerDirection.x * playerSpeed, 0, 0);
     }
 }
